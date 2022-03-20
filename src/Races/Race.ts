7234eae3dcb8fe28f82020e0ec62dea9cf274c17
thomasravache <thomasravache31@gmail.com>
2022-03-20 00:26:18 -0300

    refactor: requirement 01

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,10 +1,18 @@
 export default abstract class Race {
-  readonly name: string;
-  readonly dexterity: number;
+  private readonly _name: string;
+  private readonly _dexterity: number;
 
   constructor(name: string, dexterity: number) {
-    this.name = name;
-    this.dexterity = dexterity;
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
   }
 
   public static createdRacesInstances(): number {
